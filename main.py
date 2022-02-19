919d82b021cd56b62c3ee2c63fcc3031703b2a6d
RAGS <jragland@gmail.com>
2022-02-19 09:03:47 -0600

    removing last update

--- main.py
+++ main.py
@@ -4,8 +4,3 @@ print("Docker is magic!")
 print("No, really, it is!")
 print("Python3 is pretty cool")
 
-
-string1 = "Linux"
-string2 = "Hint"
-joined_string = string1 + string2
-print(joined_string)
