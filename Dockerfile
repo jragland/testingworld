5ba0ec13d0c468cca317e7c7b86cea917ede7929
RAGS <jragland@gmail.com>
2022-08-07 15:30:33 -0500

    Fixing Dockerfile and pre-requisite script

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,16 @@
 # For local testing - docker run --rm -it debian:buster-slim /bin/bash
 FROM ubuntu:22.04
 
+# Adding the following to give me a user inside the container with sudo
+# access
+RUN apt-get update && \
+      apt-get -y install sudo 
+
+RUN useradd -m docker && echo "docker:docker" | chpasswd && adduser docker sudo
+
+USER docker
+CMD /bin/bash
+
 # I am commenting the following in place of running an install script
 # # Run updates for Ubuntu
 # RUN apt-get -y update && apt-get -y upgrade
@@ -15,4 +25,4 @@ FROM ubuntu:22.04
 # The following will copy a script to the container for execution
 COPY scripts/linux-install.sh /tmp
 
-CMD /tmp/.linux-install.sh
+# CMD /tmp/.linux-install.sh
--- scripts/linux-install.sh
+++ scripts/linux-install.sh
@@ -3,13 +3,16 @@
 # The following script is intended to help developers get the prerequisite tools
 # installed on their system so they can setup, locally, the api-gateway repo
 
-# Perform an update of apt-get and then perform an upgrade of apt-get
+# Perform an update of apt-get and then perform an install 
+# of curl, wget, gnupg2, and libdigest-sha-perl to make your life easierf
 sudo apt-get update
-# Commenting this out at this time as we are seeing weird issues in a test EC2 instance
-# sudo apt-get -y upgrade
+sudo apt-get install -y curl
+sudo apt-get install -y wget
+sudo apt-get install -y gnupg2
+sudo apt-get install -y libdigest-sha-perl
 
-# Install packages that are needed by the api-gateway repo
-echo "Installing packages for use with api-gateway"
+# Install the following pieces of software
+echo "Installing the following packages"
 
 echo "Install kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.22.0/bin/linux/amd64/kubectl
@@ -19,15 +22,6 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 echo "Install istioctl"
 curl -sL https://istio.io/downloadIstioctl | sh -
 export PATH=$HOME/.istioctl/bin:$PATH
-
-echo "Install Go"
-sudo add-apt-repository -y ppa:longsleep/golang-backports
-sudo apt-get -y install golang-go
-
-# Currently running into isssues with this - need to continue invetigating
-echo "Install Overmind"
-sudo apt-get -y install tmux
-sudo go install github.com/DarthSim/overmind/v2@latest
 
 echo "Install jq"
 sudo apt-get -y install jq
@@ -39,9 +33,26 @@ echo "Install minikube"
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 
-# Currently running into issues with this - gpg command needs to be ran before you can add the keyring
 echo "Install k6"
-sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
-echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
 sudo apt-get update
-sudo apt-get -y install k6
+sudo apt-get install k6
+
+echo "Install Go"
+curl -OL https://go.dev/dl/go1.19.linux-amd64.tar.gz
+sudo mv ./go1.19.linux-amd64.tar.gz /usr/local
+sudo rm -rf /usr/local/go && tar -C /usr/local -xvf /usr/local/go1.19.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# Currently running into isssues with this - need to continue invetigating
+echo "Install Overmind"
+sudo apt-get -y install tmux
+wget https://github.com/DarthSim/overmind/releases/download/v2.3.0/overmind-v2.3.0-linux-amd64.gz
+gunzip -d overmind-v2.3.0-linux-amd64.gz
+sudo chmod +x overmind-v2.3.0-linux-amd64
+sudo mv overmind-v2.3.0-linux-amd64 /usr/local/bin/
+sudo ln -s /usr/local/bin/overmind-v2.3.0-linux-amd64 /usr/bin/overmind
+
+
+
