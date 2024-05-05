e56c2e331e69c9fd3428842623e78844e4fe749b
Joshua Ragland <jragland@gmail.com>
2024-05-04 19:53:23 -0500

    updating Dockerfile and script to work with amd64-x86

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,17 @@
 # For local testing:
 #     docker run --rm -it debian:buster-slim \
 #     /bin/bash
-FROM ubuntu:noble
+FROM --platform=linux/amd64 ubuntu:noble
 
 # Updating and adding of tools/apps/languages
 RUN apt-get update && \
-      apt-get -y install software-properties-common ruby-full
+      apt-get -y install software-properties-common vim sudo
 
 # Adding the following to give me a user inside
 # the container with sudo access
 RUN useradd -m docker && echo "docker:docker" | chpasswd && \
       adduser docker sudo
 USER docker
-#CMD /bin/bash
 
 # The following will copy a script to 
 # the container for execution
@@ -22,3 +21,6 @@ COPY scripts/linux-install.sh /tmp
 
 # The following will execute the copied script
 # in the container
+
+
+CMD /bin/bash
--- scripts/linux-install.sh
+++ scripts/linux-install.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e #u -o pipefail # fail on error and report it, debug all lines
+
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 
 ## This script is intended to setup multiple tools inside of a linux (debian) environment
 
@@ -9,30 +14,36 @@ sudo apt-get install -y curl wget gnupg2 libdigest-sha-perl
 
 ## Install the following pieces of software for working with kubernetes
 echo "Installing kubectl..."
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.*.0/bin/linux/amd64/kubectl
-wget https://storage.googleapis.com/kubernetes-release/release/v1.*.0/bin/linux/amd64/kubectl -q --show-progress
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo kubectl version --client --output=yaml
 
 echo "Installing minikube..."
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
-
-echo "Installing k6..."
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
-echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
-sudo apt-get update
-sudo apt-get install k6
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+sudo minikube version
 
 ## Install the following pieces of development languages and tools
-echo "Installing jq..."
-sudo apt-get -y install jq
+echo "Installing Development Languages"
+while read -r tools ; do sudo apt-get install -y $tools ; done < <(cat << "EOF"
+    jq
+    nodejs
+    ruby-full
+EOF
+)
 
-echo "Installing node..."
-sudo apt-get -y install nodejs
+# Confirm versions of installed tools
+echo "The installed version of Ruby is ..."
+sudo ruby --version
+echo "The installed version of Node is ..."
+sudo nodejs --version
+echo "The installed version of jq is ..."
+sudo jq --version
 
-echo "Installing Go..."
-curl -OL https://go.dev/dl/go1.19.linux-amd64.tar.gz
-sudo mv ./go1.19.linux-amd64.tar.gz /usr/local
-sudo rm -rf /usr/local/go && tar -C /usr/local -xvf /usr/local/go1.19.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+# echo "Installing Go..."
+# curl -OL https://go.dev/dl/go1.19.linux-amd64.tar.gz
+# sudo mv ./go1.19.linux-amd64.tar.gz /usr/local
+# sudo rm -rf /usr/local/go && tar -C /usr/local -xvf /usr/local/go1.19.linux-amd64.tar.gz
+# export PATH=$PATH:/usr/local/go/bin
